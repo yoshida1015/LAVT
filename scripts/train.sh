2e4f5ca89eeed17c3714afc303166100e9a3a2d6
ishikawa16 <shin.0116@keio.jp>
2022-07-22 23:13:23 +0900

    add --clip

--- scripts/train.sh
+++ scripts/train.sh
@@ -7,9 +7,9 @@ batch_size=$4
 mkdir ./checkpoints/reverie/$date
 
 if [ -z "$pretrain" ]; then
-    CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node 1 --master_port 12345 train.py --model lavt --dataset reverie --model_id reverie --batch-size $batch_size --lr 0.00005 --wd 1e-2 --output-dir ./checkpoints/reverie/$date/ --coat_type $coat_type --epochs 40 --img_size $img_size --use_bbox --clip --run_id $date 2>&1 | tee ./checkpoints/reverie/$date/train_log
+    CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node 1 --master_port 12345 train.py --model lavt --dataset reverie --model_id reverie --batch-size $batch_size --lr 0.00005 --wd 1e-2 --output-dir ./checkpoints/reverie/$date/ --coat_type $coat_type --epochs 40 --img_size $img_size --use_bbox --clip --run_id $date 2>&1 | tee ./checkpoints/reverie/$date/train_log
 else
-    CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node 1 --master_port 12345 train.py --model lavt --dataset reverie --model_id reverie --batch-size $batch_size --lr 0.00005 --wd 1e-2 --output-dir ./checkpoints/reverie/$date/ --coat_type $coat_type --pretrained_weights $pretrain --epochs 40 --img_size $img_size --use_bbox --clip --run_id $date 2>&1 | tee ./checkpoints/reverie/$date/train_log
+    CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node 1 --master_port 12345 train.py --model lavt --dataset reverie --model_id reverie --batch-size $batch_size --lr 0.00005 --wd 1e-2 --output-dir ./checkpoints/reverie/$date/ --coat_type $coat_type --pretrained_weights $pretrain --epochs 40 --img_size $img_size --use_bbox --clip --run_id $date 2>&1 | tee ./checkpoints/reverie/$date/train_log
 fi
 
 
