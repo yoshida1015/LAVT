38b7cecb301fcc7482d65540a6a95c78bcef8277
yoshida1015 <81620045+yoshida1015@users.noreply.github.com>
2022-08-12 01:46:01 +0900

    Update visualize_inf.sh

--- scripts/visualize_inf.sh
+++ scripts/visualize_inf.sh
@@ -2,7 +2,7 @@ date=`date -Iseconds`
 model_path=$1
 coat_type=$2
 
-mkdir ./qualitative_images/reverie/$date
+mkdir -p ./qualitative_images/reverie/$date
 
 python test.py --model lavt --coat_type $coat_type --dataset reverie --split test --resume $model_path --workers 4 --ddp_trained_weights --img_size 240 --visualize --visual_dir ./qualitative_images/reverie/$date/ --run_id $date 2>&1 | tee ./qualitative_images/reverie/$date/inf_log
 
