08064f7486cba95f0fb4d019a8dd1e752470eceb
yoshida1015 <81620045+yoshida1015@users.noreply.github.com>
2022-08-12 01:45:29 +0900

    Update inf.sh

--- scripts/inf.sh
+++ scripts/inf.sh
@@ -3,7 +3,7 @@ model_path=$1
 coat_type=$2
 img_size=$3
 
-mkdir ./log/inf/$date
+mkdir -p ./log/inf/$date
 
 python test.py --model lavt --coat_type $coat_type --dataset reverie --split test --resume $model_path --workers 4 --ddp_trained_weights --img_size $img_size --run_id $date 2>&1 | tee ./log/inf/$date/inf_log 
 
